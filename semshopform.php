6329cab27f066968c0e213c88348286b700a15e1
Edan Krolewicz <edan@beatbatmusic.com>
2017-01-05 14:23:14 -0800

    commiting form

---
+++ semshopform.php
@@ -0,0 +1,17 @@
+<form action="semshop.php" method="post" />
+<p> Product URL: <input type="url" name="product_url"/></p>
+<br>
+<p> Product SKU: <input type="text" name="product_sku"/></p>
+<br>
+<p> Product Name: <input type="text" name="product_name"/></p>
+<br>
+<p> Product Price: <input type="text" name="product_price"/></p>
+<br>
+<p> Product Date: <input type="text" name="product_date"/></p>
+<br>
+<p> Gene Category: <input type="dropdown" name="gene_category"/></p>
+<br>
+<p> Gene Name: <input type="text" name="gene_name"/></p>
+
+<input type="submit" value="Submit" />
+</form>
