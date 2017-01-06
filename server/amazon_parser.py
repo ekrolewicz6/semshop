5778af6c12b9f47e380506147e8b3f9c184ccd8d
Edan Krolewicz <edan@beatbatmusic.com>
2017-01-06 11:36:10 -0800

    amzn scraper

--- server/amazon_parser.py
+++ server/amazon_parser.py
@@ -9,8 +9,25 @@ soup = BeautifulSoup(html_contents, "lxml")
 # Find all spans
 spans = soup.find_all("span", class_="srSprite backArrow")
 #for each span, make a list of their sibling
-categories = []
+top_categories = []
 for tag in spans:
-	#Create a list of the highest level categories
-	categories.append(tag.next_sibling.string)
+	#Create a list of the top level categories (Show Results For)
+	top_categories.append(tag.next_sibling.string)
+
+# Change the scope to only the children of div id= leftnav section
+leftNav = soup.find(id="leftNav")
+
+#Find all categories
+categories = [tag.string for tag in leftNav.find_all("h2")]
+cat_tags = leftNav.find_all("h2")
+print cat_tags[0].next_sibling, "sibling"
+
+# genes_list = {}
+
+# for category in leftNav.find_all("h2"):
+# 	print category, 'category'
+# 	print category.next_sibling.children, "children"
+
+
+
 
